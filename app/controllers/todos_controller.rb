9f5e83959364432ea8701d732687d920e9121807
Josiah Rondon <josiahrondon@gmail.com>
2026-05-07 20:23:04 -0400

    made todos controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,53 @@
+class TodosController < ApplicationController
+  before_action :require_login
+
+  def index
+    @todos = current_user.todos
+  end
+
+  def new
+    @todo = current_user.todos.new
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @todo = current_user.todos.find(params[:id])
+  end
+
+  def edit
+    @todo = current_user.todos.find(params[:id])
+  end
+
+  def update
+    @todo = current_user.todos.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to todo_path(@todo)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo = current_user.todos.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def complete
+    @todos = current_user.todos.where(completed: true)
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :priority, :completed, :category_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_142037) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_001816) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
